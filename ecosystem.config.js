dbe5e032d37e026fe3f9ddd62c5a8473527a7c20
Tsoy Stanislav <statelo1992@gmail.com>
2017-01-15 06:07:34 +0900

    OMG

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       ref  : "origin/master",
       repo : "https://github.com/statelo/statelog.git",
       path : "~/statelog",
-      "post-deploy" : "nvm install && npm install && /home/statelo/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ecosystem.json --env production"
+      "post-deploy" : "nvm install && npm install && /home/statelo/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ecosystem.config.js --env production"
     }
   }
 }
