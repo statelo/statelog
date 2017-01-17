4b4a18a2349dd716807d847e24962f8144f38838
Tsoy Stanislav <statelo1992@gmail.com>
2017-01-18 05:15:29 +0900

    React Router Added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import './index.css';
 import { App, Home, About, Tutorials } from './containers/index';
 
 ReactDOM.render(
-  <Router history={ browserHistory }>
+  <Router history={ hashHistory }>
     <Route path="/" component={ App }>
       <IndexRoute component={ Home } />
       <Route path="/tutorials" component={ Tutorials } />
