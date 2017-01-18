061df126ba01a9fe6f97cf13be918a6eceabdfd4
Tsoy Stanislav <statelo1992@gmail.com>
2017-01-19 00:42:58 +0900

    History API Fallback

--- server/build/app.js
+++ server/build/app.js
@@ -8,10 +8,6 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _morgan = require('morgan');
-
-var _morgan2 = _interopRequireDefault(_morgan);
-
 var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
@@ -20,17 +16,21 @@ var _routes = require('./routes');
 
 var _routes2 = _interopRequireDefault(_routes);
 
+var _connectHistoryApiFallback = require('connect-history-api-fallback');
+
+var _connectHistoryApiFallback2 = _interopRequireDefault(_connectHistoryApiFallback);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
 var PORT = 3001;
 
-app.use((0, _morgan2.default)(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use('/api', _routes2.default);
+
+app.use((0, _connectHistoryApiFallback2.default)());
 
 app.use('/', _express2.default.static(__dirname + '/../../build'));
-
-app.use('/api', _routes2.default);
 
 app.listen(PORT, function () {
   console.log('App listening on port', PORT);
--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,17 @@
 import express from 'express';
-import morgan from 'morgan';
 import path from 'path';
 import api from './routes';
+import historyApiFallback from 'connect-history-api-fallback';
 
 const app = express();
 
 const PORT = 3001;
 
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use('/api', api);
+
+app.use(historyApiFallback());
 
 app.use('/', express.static(__dirname + '/../../build'));
-
-app.use('/api', api);
 
 app.listen(PORT, () => {
   console.log('App listening on port', PORT);
