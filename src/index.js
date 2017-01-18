71d6f1467aeb9a4918421bcb6805802776cd9890
Tsoy Stanislav <statelo1992@gmail.com>
2017-01-18 19:33:56 +0900

    webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import './index.css';
 import { App, Home, About, Tutorials } from './containers/index';
 
 ReactDOM.render(
-  <Router history={ hashHistory }>
+  <Router history={ browserHistory }>
     <Route path="/" component={ App }>
       <IndexRoute component={ Home } />
       <Route path="/tutorials" component={ Tutorials } />
