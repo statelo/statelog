d68ddf0d07c3cd42ae93fc065bcc73e8cc1a6dca
Tsoy Stanislav <statelo1992@gmail.com>
2017-02-25 04:19:31 +0900

    Whats problem?

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -42,8 +42,8 @@ class Comment extends React.Component {
             className="comment-input"
             onChange={this.handleChange}>
           </input>
-          <button onClick={this.handleDeleteComment}>DELETE</button>
-          <button onClick={this.handlePostComment}>POST</button>
+          <button onClick={this.handleDeleteComment}>Delete</button>
+          <button onClick={this.handlePostComment}>Post</button>
         </div>
         {this.props.comments.text.map((text, i) =>
           <Text key={i}>
